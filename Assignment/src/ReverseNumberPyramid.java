811970690e3b1999e197e07bd8d93642b413dc3b
Yash Agarwal <49516808+yashagrs13@users.noreply.github.com>
2024-11-10 14:48:55 +0530

    Update ReverseNumberPyramid.java

--- Assignment/src/ReverseNumberPyramid.java
+++ Assignment/src/ReverseNumberPyramid.java
@@ -8,7 +8,7 @@ public class ReverseNumberPyramid {
         int n = sc.nextInt();
 
         for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = i; j >= 1; j--) {
                 System.out.print(j + " ");
             }
             System.out.println();
